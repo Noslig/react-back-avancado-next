605f9b488ca53c5385a26fced6548a11c7585aa4
Noslig <gilsong1902@gmail.com>
2021-02-10 20:41:05 -0300

    Next

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <h1>Olá Mundo!</h1>
+    );
+  }
+}
+
+// import React, { Component } from "react";
+// import "isomorphic-fetch";
+
+// export default class Home extends Component {
+//   static getInitialProps = async () => {
+//     const response = await fetch(
+//       "https://api.github.com/orgs/rocketseat/repos"
+//     );
+
+//     return { repositories: await response.json() };
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         {this.props.repositories.map(repo => (
+//           <h1 key={repo.id}>{repo.name}</h1>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
